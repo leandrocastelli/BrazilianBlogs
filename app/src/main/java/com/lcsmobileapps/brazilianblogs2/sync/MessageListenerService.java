39edf51df0a37fbfcd7f5ea40d589a49da4c439a
leandro.silverio <leandro.silverio@lge.com>
2015-12-17 23:24:08 -0200

    GCM TEMP
    Signed-off-by: leandro.silverio <leandro.silverio@lge.com>

---
+++ app/src/main/java/com/lcsmobileapps/brazilianblogs2/sync/MessageListenerService.java
@@ -0,0 +1,69 @@
+package com.lcsmobileapps.brazilianblogs2.sync;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmListenerService;
+import com.lcsmobileapps.brazilianblogs2.BlogsActivity;
+import com.lcsmobileapps.brazilianblogs2.R;
+
+public class MessageListenerService extends GcmListenerService {
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        String message = data.getString("message");
+        Log.d("GCM", "From: " + from);
+        Log.d("GCM", "Message: " + message);
+
+        if (from.startsWith("/topics/")) {
+            // message received from some topic.
+        } else {
+            // normal downstream message.
+        }
+
+        // [START_EXCLUDE]
+        /**
+         * Production applications would usually process the message here.
+         * Eg: - Syncing with server.
+         *     - Store message in local database.
+         *     - Update UI.
+         */
+
+        /**
+         * In some cases it may be useful to show a notification indicating to the user
+         * that a message was received.
+         */
+        sendNotification(message);
+        // [END_EXCLUDE]
+    }
+
+
+    private void sendNotification(String message) {
+        Intent intent = new Intent(this, BlogsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_media_play)
+                .setContentTitle("GCM Message")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
